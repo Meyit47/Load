36889d1fbfa58b2b146704e7adf94a83ca512922
Meyit47 <silaatesss.31@gmail.com>
2023-11-29 15:32:33 +0300

    Update rvt.sh

--- rvt.sh
+++ rvt.sh
@@ -20,19 +20,19 @@ echo " "
 echo " "
 echo -e "\e[1m\e[31m\
            
-           ████████╗   ██╗      ██████╗  █████╗ ██████╗
-           ╚══██╔══╝   ██║     ██╔═══██╗██╔══██╗██╔══██╗
-              ██║█████╗██║     ██║   ██║███████║██║  ██║
-              ██║╚════╝██║     ██║   ██║██╔══██║██║  ██║ reverter {66}
-              ██║      ███████╗╚██████╔╝██║  ██║██████╔╝
-              ╚═╝      ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ v 1.1"
+                         ██╗      ██████╗  █████╗ ██████╗
+                         ██║     ██╔═══██╗██╔══██╗██╔══██╗
+                         ██║     ██║   ██║███████║██║  ██║
+                         ██║     ██║   ██║██╔══██║██║  ██║ geri döndürücü {66}
+                         ███████╗╚██████╔██║  ██║██████╔╝
+                         ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ v 1.1"
 echo " "  
-echo -e  "                  \e[1m\e[32m▂▃▄▅▆▇▓▒░Coded By \e[31mN17R0 \e[1m\e[32m░▒▓▇▆▅▄▃▂"
+echo -e  "                  \e[1m\e[32m▂▃▄▅▆▇▓▒░Kodlayan \e[31mN17R0 \e[1m\e[32m░▒▓▇▆▅▄▃▂"
 echo "                      -------------------------"
-echo -e  "            \e[1m\e[32m--------->[\e[31myoutube.com/noobhackers\e[1m\e[32m]<----------"  
+echo -e  "            \e[1m\e[32m--------->[\e[31mt.me/Meyit47\e[1m\e[32m]<----------"  
 echo " "
 echo ""
-echo "                           rverting wait for 10s please"
+echo "                           geri dönüyoruz 10 saniye bekleyin lütfen"
 cd $HOME
 cd T-LOAD
 cd revert 
@@ -42,8 +42,8 @@ rm bash.bashrc
 cd $HOME
 
 mv bash.bashrc /data/data/com.termux/files/usr/etc
-echo "             "EXIT FROM TERMUX AND RE OPEN IT AFTER 5 SECONDS |lolcat 
+echo "             "TERMUX TAN ÇIKIN VE 5 SANİYE SONRA YENİDEN AÇIN |lolcat 
 echo " "
-echo  "            reverted successfully now you can use.....termux normally"
+echo  "            başarıyla geri döndürüldü, artık normal şekilde .....termux u kullanabilirsiniz"
 sleep 3.0
 cd $HOME
